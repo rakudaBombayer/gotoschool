c7b4a9dbbc998d891edfc8c0049e69fbf4499140
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-05-13 20:15:15 +0900

    復元
    復元

---
+++ Assets/Playercontroller.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{   
+    public Transform Camera;
+    public float PlayerSpeed;
+    public float RotationSpeed;
+
+    Vector3 speed = Vector3.zero;
+    Vector3 rot = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Move();
+        Rotation();
+        Camera.transform.position = transform.position;
+    }
+
+    void Move()
+    {
+        speed = Vector3.zero;
+        rot = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            // speed.z = PlayerSpeed;
+            rot.y = 0;
+            MoveSet();
+        }        
+        if(Input.GetKey(KeyCode.S))
+        {
+            // speed.z = PlayerSpeed;
+            rot.y = 180;
+            MoveSet();
+        }        
+        if(Input.GetKey(KeyCode.A))
+        {
+            // speed.z = PlayerSpeed;
+            rot.y = -90;
+            MoveSet();
+        }        
+        if(Input.GetKey(KeyCode.D))
+        {
+            // speed.z = PlayerSpeed;
+            rot.y = 90;
+            MoveSet();
+        }
+        // transform.eulerAngles = Camera.transform.eulerAngles + rot;
+        transform.Translate(speed); 
+    }
+
+    void MoveSet()
+    {
+        speed.z = PlayerSpeed;
+        transform.eulerAngles = Camera.transform.eulerAngles + rot;
+    }
+
+    void Rotation()
+    {
+        var speed = Vector3.zero;
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed.y = -RotationSpeed;
+        }        
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            speed.y = RotationSpeed;
+        }
+
+        Camera.transform.eulerAngles += speed;
+    }
+}
---
+++ Assets/enemycontroler.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemycontroler : MonoBehaviour
+{   
+    // public float time;死ぬまでの時間
+    float Timer;
+    public float ChangeTime;
+    public float EnemySpeed;
+
+    GameObject Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroy(this.gameObject, time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var speed = Vector3.zero;
+        speed.z = EnemySpeed;
+        var rot = transform.eulerAngles;
+
+        if (Target)
+        {
+            transform.LookAt(Target.transform);
+            this.transform.Translate(speed);
+            // 上にこれがあれば追いかけやめ スイッチ アクション動画#10 12:00ごろ
+        }
+        else
+        {
+            Timer += Time.deltaTime;
+            if (ChangeTime <= Timer)
+            {
+                float rand = Random.Range(0, 360);
+                rot.y = rand;
+                Timer = 0;
+            }
+        }
+
+        
+        rot.x = 0;
+        rot.z = 0;
+        transform.eulerAngles = rot;
+
+        // this.transform.Translate(speed);
+        // 上にこれがあれば常に走る スイッチ
+    }
+
+    private void OnTriggerEnter(Collider other) 
+        {
+            if(other.tag=="Player")
+            {
+                Target = other.gameObject;
+            }
+        }
+
+    private void OnTriggerExit(Collider other) 
+        {
+            if(other.tag=="Player")
+            {
+                Target = null;
+            }
+        }
+
+}
+
+
